4818651da6ae39e6738b5def36d3ceeda83af40c
Your Actual Name <Your Actual Email>
2014-04-22 21:15:43 -0700

    fucken backup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Etsy::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #DEVISE
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Etsy::Application.routes.draw do
+  devise_for :users
   resources :listings
 
   get "pages/about"
