41c039792cee30cef0ffc8aeb9a4f409666c39fe
Your Actual Name <Your Actual Email>
2014-04-24 18:20:02 -0700

    sellers page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,11 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
+
+def seller
+    @listings = Listing.where(user: current_user).order("created_at DESC")
+  end
 
   # GET /listings
   # GET /listings.json
@@ -71,4 +77,10 @@ class ListingsController < ApplicationController
     def listing_params
        params.require(:listing).permit(:name, :description, :price, :image)
     end
+
+    def check_user
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
+    end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Etsy::Application.routes.draw do
 
   get "pages/about"
   get "pages/contact"
+  get 'seller' =>"listings#seller"
 
   root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
